b9f98435b00179b941537d0217cc6a2c031084f2
KevinSipioni <kevinsipioni13@gmail.com>
2024-07-23 23:42:34 -0300

    ninth commit

--- ARRAYS E SLICES/arrays-internos.go
+++ ARRAYS E SLICES/arrays-internos.go
@@ -2,7 +2,7 @@ package main
 
 import ("fmt")
 
-func main1() {
+func main() {
 	fmt.Println("Arrays e Slices")
 
 	var array1 [5] string
@@ -26,4 +26,23 @@ func main1() {
 
 	array2[1] = "Posição 2 alterada"
 	fmt.Println(slice2)
+
+	// Arrays internos
+
+	slice3 := make([]float32, 10, 15)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
